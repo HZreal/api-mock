7ec6a710171c100640014d07fb8022342be9140e
HZreal <huangzhen_happy@163.com>
2024-09-20 20:30:55 +0800

    feat: add sql file

---
+++ public/table.sql
@@ -0,0 +1,14 @@
+create table tb_api
+(
+    id           int auto_increment
+        primary key,
+    name         varchar(16)  null,
+    uri          varchar(64)  null,
+    args         varchar(128) null,
+    method       varchar(8)   null,
+    params       json         null,
+    content_type varchar(32)  null,
+    uri_args     varchar(64)  null
+)
+    comment '接口表';
+
