c96a7a2df535b5d2e970a45949c4e3e62b18e9bc
HZreal <huangzhen_happy@163.com>
2024-09-24 09:33:51 +0800

    chore:

--- public/table.sql
+++ public/table.sql
@@ -2,13 +2,13 @@ create table tb_api
 (
     id           int auto_increment
         primary key,
-    name         varchar(16)  null,
-    uri          varchar(64)  null,
-    args         varchar(128) null,
+    name         varchar(16)  null comment '名称',
+    uri          varchar(128) null comment '路径',
+    args         varchar(128) null comment '路径参数',
     method       varchar(8)   null,
-    params       json         null,
+    params       json         null comment '参数',
     content_type varchar(64)  null,
-    uri_args     varchar(128)  null
+    uri_args     varchar(256) null comment '带 args 路径',
+    body         varchar(512) null
 )
     comment '接口表';
-
